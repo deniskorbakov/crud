5133fbb0aadb5ef481c6b63ff5a21821305b600e
deniskorbakov <korbakovd@gmail.com>
2023-02-17 21:03:16 +0300

    обновил бутстрап

--- crud.php
+++ crud.php
@@ -39,12 +39,7 @@ $sql = "SELECT * FROM `users` WHERE `login` = '$login'";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link
-        rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
-        integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I"
-        crossorigin="anonymous"
-    >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>CRUD</title>
 </head>
 <body class="bg-dark">
@@ -54,5 +49,6 @@ $sql = "SELECT * FROM `users` WHERE `login` = '$login'";
             <a href="personalAcc.php" class="btn btn-success text-right mt-5">Личный кабинет</a>
         </div>
     </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,12 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link
-        rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
-        integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I"
-        crossorigin="anonymous"
-    >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Регистрация</title>
 </head>
 <body class="bg-dark">
@@ -23,10 +18,11 @@
                     <input class="form-control p-2" name="login" type="text" placeholder="Введите логин">
                     <input class="form-control p-2 mt-4" name="password" type="password" placeholder="Введите пароль">
                     <button class="btn btn-success p-2 mt-4" type="submit">Зарегистрироваться</button>
-                    <a href="authorization.php" class="btn btn-danger p-2 mt-4 ml-4">Перейти на авторизацию</a>
+                    <a href="authorization.php" class="btn btn-danger p-2 mt-4 ms-2">Перейти на авторизацию</a>
                 </form>
             </div>
         </div>
     </main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
