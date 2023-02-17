725d46ea9afa331ee448db7789fbca6d20eabb12
deniskorbakov <korbakovd@gmail.com>
2023-02-17 20:35:01 +0300

    подправил код и хеширую токен

--- backend/authorizationBack.php
+++ backend/authorizationBack.php
@@ -3,35 +3,35 @@
 $login = $_POST['login'];
 $password = $_POST['password'];
 
-
 include_once '../connection/connectMySql.php';
-
-
 
 if(strlen($login) <= 5){
     echo "<h3>Введите логин!</h3>" . header("refresh:3;url=../authorization.php");
 }
 else if(strlen($password) < 8) {
-    echo "<h3>Введите пароль!</h3>". var_dump($password);
+    echo "<h3>Введите пароль!</h3>" . header("refresh:3;url=../authorization.php");
 }
 else{
+
 $sql = "SELECT * FROM `users` WHERE `login` = '$login'";
 if($result = $mysql->query($sql)){
     foreach($result as $row) {
     $userlogin = $row["login"];
-    $userpassword = $row["password"]; 
-    $usertoken = $row["token"];
+    $userPassword = $row["password"]; 
+    $userToken = $row["token"];
     }
-    if(empty($usertoken)) {
+    if(empty($userToken)) {
         echo "<h3>Вы не зарегистрированы</h3>".header("refresh:3;url=../authorization.php");
     }
     else if($login = $userlogin) {
-        if(password_verify($password, $userpassword)) {
+        if(password_verify($password, $userPassword)) {
+            
+            //хешируем токен для безопастности
+            $token = password_hash($userToken, PASSWORD_BCRYPT, ['cost' => 12,]);
 
-            $mysql->close();
-
-            setcookie('user', $login, time() + (10 * 365 * 24 * 60 * 60), "/");
-
+            setcookie('login', $login, time() + (10 * 365 * 24 * 60 * 60), "/");
+            setcookie('token', $token, time() + (10 * 365 * 24 * 60 * 60), "/");
+            
             header("Refresh:0; url=../crud.php");
         }
         else {
