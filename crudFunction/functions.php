edc999e828132a5211e683cfb8986a1bafb8fded
deniskorbakov <korbakovd@gmail.com>
2023-02-18 21:18:07 +0300

    добавил все функции

--- crudFunction/functions.php
+++ crudFunction/functions.php
@@ -6,13 +6,50 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
+$get_id = $_GET['id'];
+
+$login = $_COOKIE['login'];
+$token = '';
+
+$sql = "SELECT * FROM `users` WHERE `login` = '$login'";
+
+    if($result = $mysql->query($sql)) {
+
+        foreach($result as $row) {
+            $userToken = $row["token"];
+        }
+
+        $token = $userToken;        
+    }
+
+var_dump($token);
+//read users
+$result = $mysql->query("SELECT * FROM `crudUsers` WHERE `flag` = '0' AND `token` = '$token'");
+
+//add users
 if(isset($_POST['add'])) {
-    $query = $mysql->query("INSERT INTO `crudUsers` (`created_at`, `updated_at`, `name`, `email`, `password`) VALUES (NOW(),NOW(),'$name','$email','$password')");
+    $query = $mysql->query("INSERT INTO `crudUsers` (`created_at`, `updated_at`, `name`, `email`, `password`, `token`, `flag`) VALUES (NOW(),NOW(),'$name','$email','$password','$token','0')");
 
     if($query) {
         header("Location: " . $_SERVER['HTTP_REFERER']);
     }
 }
 
-$result = $mysql->query("SELECT * FROM `crudUsers`");
+//update users
+if(isset($_POST['edit'])) {
+    $query = $mysql->query("UPDATE `crudUsers` SET `updated_at`=NOW(), `name` = '$name', `email` = '$email', `password` = '$password' WHERE `id` = '$get_id' AND `token` = '$token'");
+
+    if($query) {
+        header("Location: " . $_SERVER['HTTP_REFERER']);
+    }
+}
+
+//delete users
+if(isset($_POST['delete'])) {
+    $query = $mysql->query("UPDATE `crudUsers` SET `flag` = '1' WHERE `id` = '$get_id' AND `token` = '$token'");
+
+    if($query) {
+        header("Location: " . $_SERVER['HTTP_REFERER']);
+    }
+}
 
