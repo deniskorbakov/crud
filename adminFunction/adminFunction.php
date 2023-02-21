1bf95886dfd9128f744732db48396791125972a9
deniskorbakov <korbakovd@gmail.com>
2023-02-21 23:27:05 +0300

    исправил логику инпутов

--- adminFunction/adminFunction.php
+++ adminFunction/adminFunction.php
@@ -8,31 +8,76 @@ $password = $_POST['password'];
 //получаем айди для работы с полем по айди
 $get_id = $_GET['id'];
 
-//хеширование пароля
-$password = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12,]);
-
 //read users
 $result = $mysql->query("SELECT * FROM `users` WHERE `flag` = 0");
 
 //add users
 if(isset($_POST['add'])) {
-    //генирация токена
-    $permitted_chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $token = substr(str_shuffle($permitted_chars), 0, 10);
-
-    $query = $mysql->query("INSERT INTO `users` (`login`,`password`,`token`,`flag`) VALUES ('$login','$password','$token','0')");
-
-    if($query) {
+    if(preg_match('/[А-Я а-яЁё.,]/u', $login)) {
         header("Location: " . $_SERVER['HTTP_REFERER']);
-    }
+      }
+      else if(preg_match('/[А-Я а-яЁё]/u', $password)) {
+        header("Location: " . $_SERVER['HTTP_REFERER']);
+      }
+      else{
+         //проверка на пустоту и введенных данных
+          if(strlen($login) <= 5 || strlen($login) > 15) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else if(strlen($password) < 8 || strlen($password) > 20) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else if($login == $password) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else {
+                //генирация токена
+                $permitted_chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                $token = substr(str_shuffle($permitted_chars), 0, 10);
+
+                //хеширование пароля
+                $password = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12,]);
+
+                $query = $mysql->query("INSERT INTO `users` (`login`,`password`,`token`,`flag`) VALUES ('$login','$password','$token','0')");
+
+                if($query) {
+                    header("Location: " . $_SERVER['HTTP_REFERER']);
+                }
+          }
+      }
+    
 }
 
 //update users
 if(isset($_POST['edit'])) {
-    $query = $mysql->query("UPDATE `users` SET `login` = '$login', `password` = '$password' WHERE `id` = '$get_id'");
 
-    if($query) {
+
+    if(preg_match('/[А-Я а-яЁё.,]/u', $login)) {
         header("Location: " . $_SERVER['HTTP_REFERER']);
+      }
+      else if(preg_match('/[А-Я а-яЁё]/u', $password)) {
+        header("Location: " . $_SERVER['HTTP_REFERER']);
+      }
+      else{
+         //проверка на пустоту и введенных данных
+          if(strlen($login) <= 5 || strlen($login) > 15) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else if(strlen($password) < 8 || strlen($password) > 20) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else if($login == $password) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else {
+    
+            $password = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12,]);
+            $query = $mysql->query("UPDATE `users` SET `login` = '$login', `password` = '$password' WHERE `id` = '$get_id'");
+
+            if($query) {
+                header("Location: " . $_SERVER['HTTP_REFERER']);
+            }
+        }
     }
 }
 
--- crudFunction/functions.php
+++ crudFunction/functions.php
@@ -29,20 +29,66 @@ $result = $mysql->query("SELECT * FROM `crudUsers` WHERE `flag` = '0' AND `token
 
 //add users
 if(isset($_POST['add'])) {
-    $query = $mysql->query("INSERT INTO `crudUsers` (`created_at`, `updated_at`, `name`, `email`, `password`, `token`, `flag`) VALUES (NOW(),NOW(),'$name','$email','$password','$token','0')");
-
-    if($query) {
+    if(preg_match('/[А-Я а-яЁё.,]/u', $name)) {
         header("Location: " . $_SERVER['HTTP_REFERER']);
-    }
+      }
+      else if(preg_match('/[А-Я а-яЁё]/u', $password)) {
+        header("Location: " . $_SERVER['HTTP_REFERER']);
+      }
+      else{
+         //проверка на пустоту и введенных данных
+          if(strlen($name) <= 5 || strlen($name) > 15) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else if(strlen($password) < 8 || strlen($password) > 20) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else if($name == $password) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else {
+            $query = $mysql->query("INSERT INTO `crudUsers` (`created_at`, `updated_at`, `name`, `email`, `password`, `token`, `flag`) VALUES (NOW(),NOW(),'$name','$email','$password','$token','0')");
+
+            if($query) {
+                header("Location: " . $_SERVER['HTTP_REFERER']);
+            }
+          }
+        }
 }
 
 //update users
 if(isset($_POST['edit'])) {
-    $query = $mysql->query("UPDATE `crudUsers` SET `updated_at`=NOW(), `name` = '$name', `email` = '$email', `password` = '$password' WHERE `id` = '$get_id' AND `token` = '$token'");
-
-    if($query) {
+    if(preg_match('/[А-Я а-яЁё.,]/u', $name)) {
         header("Location: " . $_SERVER['HTTP_REFERER']);
-    }
+      }
+      else if(preg_match('/[А-Я а-яЁё]/u', $password)) {
+        header("Location: " . $_SERVER['HTTP_REFERER']);
+      }
+      else{
+         //проверка на пустоту и введенных данных
+          if(strlen($name) <= 5 || strlen($name) > 15) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else if(strlen($password) < 8 || strlen($password) > 20) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else if($name == $password) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+          }
+          else {
+            $query = $mysql->query("UPDATE `crudUsers` SET `updated_at`=NOW(), `name` = '$name', `email` = '$email', `password` = '$password' WHERE `id` = '$get_id' AND `token` = '$token'");
+
+            if($query) {
+                header("Location: " . $_SERVER['HTTP_REFERER']);
+            }
+        }
+      }
 }
 
 //delete users
